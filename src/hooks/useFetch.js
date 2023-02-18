969fffb5883483db6727ad0a6b6b0a14cf010447
EMMANUEL <dammylola299@gmail.com>
2023-02-18 05:42:40 +0100

    Implemented a custom hook to fetch data from API

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,22 @@
+// All react imports
+import { useEffect, useState } from "react";
+
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(jsonData => {
+                // console.log(jsonData);
+                setData(jsonData.results);
+            })  
+        
+    }, [url])
+
+    return { data };
+
+}
+ 
+export default useFetch;
