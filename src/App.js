1db5cf23a48acccffc1f9009f7602ebc6c03613d
Damilola <damiemmanuel@tekpirate.com>
2023-02-21 10:26:39 +0100

    Worked on the search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 // All react imports
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 // All styles/Bootstrap imports
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav, Form, FormControl, Button } from 'react-bootstrap';
-// All hooks import
-import useFetch from './hooks/useFetch';
 // All components import
 import MovieCard from "./components/MovieCard";
 
@@ -15,14 +13,39 @@ const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=4e9fca429e4a
 // const API_SEARCH="https://api.themoviedb.org/3/search/movie?api_key=4e9fca429e4ad82c14ea0eb808059545&language=en-US&query";
 
 function App() {
-  const { data: movies } = useFetch(API_URL);
+  const [movies, setMovies] = useState([]);
   const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    fetch(API_URL)
+        .then(res => res.json())
+        .then(jsonData => {
+            // console.log(jsonData.results);
+            setMovies(jsonData.results);
+        })
+    
+  }, [])
+
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    console.log('searching...');
+    try {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=4e9fca429e4ad82c14ea0eb808059545&language=en-US&query=${query}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setMovies(data.results);
+    }
+    catch(e) {
+      console.log(e.message);
+    }
+  }
 
   return (
     <>
       <Navbar bg="dark" expand="lg" variant="dark">
         <Container fluid>
-          <Navbar.Brand href="/home" >MovieDb App</Navbar.Brand>
+          <Navbar.Brand href="/home" >Movie App</Navbar.Brand>
           <Navbar.Brand href="/home">Trending</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
 
@@ -32,7 +55,7 @@ function App() {
                 className="me-auto my-2 my-lg-3"
                 navbarScroll
               ></Nav>
-              <Form className='d-flex'>
+              <Form className='d-flex' onSubmit={searchMovie}>
                 <FormControl
                   type='search'
                   placeholder='movie search'
@@ -49,7 +72,7 @@ function App() {
       </Navbar>
       <div className='container'>
         <div className="grid">
-          { movies && movies.map(movie => (
+          { movies.map(movie => (
               <MovieCard key={movie.id} {...movie} />    
           )) }
         </div>
